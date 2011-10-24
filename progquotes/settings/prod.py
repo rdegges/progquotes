3622c44f6a550c7570450c67a437b79a977b7467
Randall Degges <rdegges@gmail.com>
2011-10-23 18:57:02 -0700

    Adding settings.prod file.

--- progquotes/settings/prod.py
+++ progquotes/settings/prod.py
@@ -14,7 +14,7 @@ EMAIL_HOST_USER = environ['EMAIL_HOST_USER']
 EMAIL_PORT = 587
 EMAIL_SUBJECT_PREFIX = '[%s]' % SITE_NAME
 EMAIL_USE_TLS = False
-SERVER_EMAIL = 'root@localhost'
+SERVER_EMAIL = environ['EMAIL_HOST_USER']
 ########## END EMAIL CONFIGURATION
 
 
@@ -22,3 +22,16 @@ SERVER_EMAIL = 'root@localhost'
 # Our secret key.
 SECRET_KEY = environ['SECRET_KEY']
 ########## END SECRET CONFIGURATION
+
+
+########## CACHE CONFIGURATION
+CACHES = {
+    # Memcached cache. See
+    # http://docs.djangoproject.com/en/1.3/topics/cache/#memcached for more
+    # information.
+    'default': {
+       'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+       'LOCATION': environ['MEMCACHE_SERVERS'] + ':11211',
+    }
+}
+########## END CACHE CONFIGURATION
