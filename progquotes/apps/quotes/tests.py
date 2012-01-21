5e7f7f911f78ff9c947856b334a66c28bd0e34b3
Randall Degges <rdegges@gmail.com>
2012-01-21 13:43:58 -0800

    Adding some basic __unicode__ tests for quotes.models.Quote.

--- progquotes/apps/quotes/tests.py
+++ progquotes/apps/quotes/tests.py
@@ -1,16 +1,27 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+"""Application tests."""
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from django.utils import unittest
+
+from apps.quotes.models import Quote
+
+
+class QuoteTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.quote = Quote.objects.create(
+            author = 'Randall Degges',
+            body = 'This is a test quote.'
+        )
+
+    def test_unicode_returns_unicode(self):
+        self.assertIsInstance(self.quote.__unicode__(), unicode)
+
+    def test_unicode_contains_body(self):
+        self.assertTrue(self.quote.body in self.quote.__unicode__())
+
+    def test_unicode_contains_author(self):
+        self.assertTrue(self.quote.author in self.quote.__unicode__())
+
+    def tearDown(self):
+        Quote.objects.all().delete()
