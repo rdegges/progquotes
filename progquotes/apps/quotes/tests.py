544c69b5f50dcaa45787aba87eb6cf7cc80ee175
Randall Degges <rdegges@gmail.com>
2012-01-21 13:52:25 -0800

    Adding tests for custom ``save`` method.
    These tests provide the foundation for our custom save method.
    
    We'll use save() to set the submitted and updated time fields, respectively.

--- progquotes/apps/quotes/tests.py
+++ progquotes/apps/quotes/tests.py
@@ -23,5 +23,21 @@ class QuoteTestCase(unittest.TestCase):
     def test_unicode_contains_author(self):
         self.assertTrue(self.quote.author in self.quote.__unicode__())
 
+    def test_save_sets_submitted_time(self):
+        self.assertTrue(self.quote.submitted)
+
+    def test_save_sets_updated_time(self):
+        self.assertTrue(self.quote.updated)
+
+    def test_save_only_sets_submitted_time_on_creation(self):
+        time = self.quote.submitted
+        self.quote.save(force_update=True)
+        self.assertEquals(time, self.quote.submitted)
+
+    def test_save_updates_updated_time_each_save(self):
+        time = self.quote.updated
+        self.quote.save(force_update=True)
+        self.assertNotEquals(time, self.quote.updated)
+
     def tearDown(self):
         Quote.objects.all().delete()
