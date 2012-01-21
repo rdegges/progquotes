0555e318d966505c85b40b1b01071d08ebd0e03a
Randall Degges <rdegges@gmail.com>
2012-01-21 13:53:09 -0800

    Create quote.models.Quote.save() method.
    This method sets the submitted and updated times whenever a Quote is saved.

--- progquotes/apps/quotes/models.py
+++ progquotes/apps/quotes/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 from caching.base import CachingManager, CachingMixin
@@ -14,7 +16,7 @@ class Quote(CachingMixin, models.Model):
     author = models.CharField(blank=True, max_length=255)
     body = models.TextField()
     date = models.DateField(blank=True, null=True)
-    slug = models.SlugField(primary_key=True)
+    slug = models.SlugField()
     source = models.TextField(blank=True)
     submitted = models.DateTimeField(editable=False)
     submitted_by = models.EmailField()
@@ -22,3 +24,15 @@ class Quote(CachingMixin, models.Model):
 
     def __unicode__(self):
         return u'%s - %s' % (self.body, self.author)
+
+    def save(self, *args, **kwargs):
+        """Automatically populate the ``submitted`` and ``updated`` datetime
+        fields.
+        """
+        current_time = datetime.now()
+
+        if not self.pk:
+            self.submitted = current_time
+
+        self.updated = current_time
+        super(Quote, self).save(*args, **kwargs)
