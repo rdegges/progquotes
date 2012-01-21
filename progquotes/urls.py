75eb5d2005d7da0bb4d5a233a0430ea22be19c2f
Randall Degges <rdegges@gmail.com>
2012-01-21 01:46:57 -0800

    Making / render our index.html template.

--- progquotes/urls.py
+++ progquotes/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'progquotes.views.home', name='home'),
-    # url(r'^progquotes/', include('progquotes.foo.urls')),
+    url(r'^$', direct_to_template, {'template': 'index.html'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
