8122bdac7522119513a32cf33e8846e1f3a890ff
Randall Degges <rdegges@gmail.com>
2012-01-20 23:18:41 -0800

    Classing up settings.dev module.

--- progquotes/settings/dev.py
+++ progquotes/settings/dev.py
@@ -1,4 +1,4 @@
-"""Development settings."""
+"""Development settings and globals."""
 
 
 from os.path import join, normpath
@@ -7,17 +7,22 @@ from common import *
 
 
 ########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#debug
 DEBUG = True
+
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
 ########## END DEBUG CONFIGURATION
 
 
 ########## EMAIL CONFIGURATION
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ########## END EMAIL CONFIGURATION
 
 
 ########## DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -32,6 +37,7 @@ DATABASES = {
 
 
 ########## CACHE CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#caches
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
